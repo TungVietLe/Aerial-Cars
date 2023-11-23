4a8c6015f9f4a033e0ce4e3762c6580266099e13
TungVLe <tunle0801@gmail.com>
2023-11-23 10:02:33 -0500

    trained `best_train4.pt` with improved accuracy

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import cv2
 
 
 # load yolov8 model
-model = YOLO("best.pt")
-model.fuse()
+model = YOLO("best_train4.pt")
+
 
 # cap = cv2.imread("dining.png")
 
@@ -24,10 +24,10 @@ while ret:
     ret, frame = cap.read()
 
     if ret:
+        frame = frame[200:900, :]  # roi
         # detect objects
         # track objects
-        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        results = model.predict(frame)
+        results = model.track(frame, persist=True)
 
         # plot results
         # cv2.rectangle
@@ -35,6 +35,7 @@ while ret:
         frame_ = results[0].plot()
 
         # visualize
+        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
         cv2.imshow("frame", frame_)
         if cv2.waitKey(0) == ord("q"):
             break
--- train.py
+++ train.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 
 # Load a model
 
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("best_train3.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="data.yaml", epochs=7, patience=3, imgsz=416)  # train the model
-metrics = model.val()  # evaluate model performance on the validation set
+model.train(data="data.yaml", epochs=6, patience=3)  # train the model
+# metrics = model.val()  # evaluate model performance on the validation set
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 path = model.export()  # export the model to ONNX format
